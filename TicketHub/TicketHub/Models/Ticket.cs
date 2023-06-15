37c3021391959b5053305bd7adf3d3965c15a5e3
justsliepins <justsliepins0@gmail.com>
2023-06-15 16:50:06 +0300

    creating database models and controllers with views

---
+++ TicketHub/TicketHub/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketHub.Models
+{
+	public class Ticket
+	{
+		public int Id { get; set; }
+		public int EventId { get; set; }
+		public int SellerId { get; set; }
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+	}
+}
---
+++ TicketHub/TicketHub/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketHub.Models
+{
+	public class User
+	{
+		[Key]
+		[Column("ID")]
+		public int Id { get; set; }
+		[Column("first_name")]
+		[StringLength(50)]
+		public string Name { get; set; }
+		[Column("last_name")]
+		[StringLength(50)]
+		public string Surname { get; set; }
+		[Column("alias")]
+		[StringLength(30)]
+		public string Alias { get; set; }
+		[Column("email")]
+		[StringLength(255)]
+		public string Email { get; set; }
+		[Column("password")]
+		[StringLength(255)]
+		public string Password { get; set; }
+		[Column("is_admin")]
+		public bool isAdmin { get; set; }
+	}
+}
